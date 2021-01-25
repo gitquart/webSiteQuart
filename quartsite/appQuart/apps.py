39b3aa18439976cdcd5717b7b5a92e7e2df688d1
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-25 10:49:16 -0600

    ...

---
+++ quartsite/appQuart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppquartConfig(AppConfig):
+    name = 'appQuart'
--- quartsite/quartsite/urls.py
+++ quartsite/quartsite/urls.py
@@ -18,4 +18,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('appQuart/', include('appQuart.urls'))
 ]
+
+
+
+
