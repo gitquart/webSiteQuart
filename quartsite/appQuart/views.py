62f49a5c714f1d8b97a792c966337d5c8b73ee55
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-26 10:17:02 -0600

    ...

--- quartsite/appQuart/views.py
+++ quartsite/appQuart/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import views
+import view
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the quart index.")
+    return index
--- quartsite/quartsite/urls.py
+++ quartsite/quartsite/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('admin/', admin.site.urls),
-    path('appQuart/', include('appQuart.urls'))
+    #path('', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('',include('appQuart.urls') )
     
 ]
 
