d386dc7798bd5af3ad6f58127fe8e8d21984a23e
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-29 09:23:31 -0600

    ...

--- quartsite/quartsite/settings.py
+++ quartsite/quartsite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'quartsite.urls'
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
   '/static/',
   os.path.join(BASE_DIR,'static')
 ]
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
